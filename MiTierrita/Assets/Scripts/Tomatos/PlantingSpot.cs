3744dbe56a928b6e6e10b750e2b475b3edf13a27
Maria Angélica Cuadrado Rodriguez <angel.kira65@gmail.com>
2026-04-12 19:41:33 -0400

    Seeding working

--- MiTierrita/Assets/Scripts/Tomatos/PlantingSpot.cs
+++ MiTierrita/Assets/Scripts/Tomatos/PlantingSpot.cs
@@ -1,17 +1,35 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlantingSpot : MonoBehaviour
 {
     [SerializeField] private GameObject plantPrefab;
-
     private TomatoPlant currentPlant;
 
-    public void Plant() 
+    private void OnTriggerEnter(Collider other)
     {
-        if (currentPlant == null) 
+        Debug.Log("⚠️ ¡Algo entró en la tierra! Objeto: " + other.gameObject.name + " | Tag: " + other.tag);
+
+        if (other.CompareTag("Seed"))
         {
-            GameObject newPLant = Instantiate(plantPrefab);
-            currentPlant = newPLant.GetComponent<TomatoPlant>();
+            if (currentPlant == null)
+            {
+                Debug.Log("🌱 ¡Semilla correcta detectada! Plantando...");
+                Destroy(other.gameObject);
+                Plant();
+            }
+            else
+            {
+                Debug.Log("❌ Cayó una semilla, pero YA HAY una planta aquí.");
+            }
+        }
+    }
+
+    public void Plant()
+    {
+        if (currentPlant == null)
+        {
+            GameObject newPlant = Instantiate(plantPrefab, transform.position, Quaternion.identity);
+            currentPlant = newPlant.GetComponent<TomatoPlant>();
         }
     }
 }
